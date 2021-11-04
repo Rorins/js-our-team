11cdce0ad8b039254a5cad67872a576377d7220d
Aurora Grippaudo <77445911+Rorins@users.noreply.github.com>
2021-11-04 18:45:43 +0100

    finito

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const membri = [
   },
 ];
 
-console.table(membri);
+
 
 const teamContainer = document.querySelector(".team-container")
 
@@ -47,21 +47,52 @@ for(i = 0; i < membri.length; i++){
     const membro = membri[i];
     console.log(membro);
 
-    //stampiamo i nostri membri e eventuali contenuti dentro l'html
-    teamContainer.innerHTML += `
-    <div class="team-card">
-    <div class="card-image">
-    <img src=${membro.immagine}/>
-    </div>
-    <div class="card-text">
-    <h3>${membro.nome}</h3>
-     <p>${membro.ruolo}</p>
-    </div>
-  </div> ` 
+printatrice(membro);
+   
 }
+
+// Click bottone add
+const add = document.querySelector("button");
+
+//aggiunto la mia funzione
+add.addEventListener("click", () => nuovoMembro())
+
+
+
+
+
+/////FUNZIONE/////
 
 //Aggiunta nuovi membri team usando una funzione
 function nuovoMembro(){
-    //List membri
+// la funzione prende i valori e ci crea un oggetto
+const nuovoMembro =
+{
+immagine : document.getElementById("image").value,
+nome : document.getElementById("name").value,
+ruolo : document.getElementById("role").value
+};
 
+//Pushiamo l'oggetto nell'array membri
+membri.push(nuovoMembro)
+
+//inception di funzione printatrice
+printatrice(nuovoMembro)
+}
+
+//Funzone printatrice
+//stampiamo i nostri membri e eventuali contenuti dentro l'html
+function printatrice(teamMember){
+teamContainer.innerHTML += `
+<div class="team-card">
+<div class="card-image">
+<img src=${teamMember.immagine}/>
+</div>
+<div class="card-text">
+<h3>${teamMember.nome}</h3>
+ <p>${teamMember.ruolo}</p>
+</div>
+</div> ` 
+
+console.table(membri)
 }
